0a94dd0792f6f1a80130b627aaf126ea0d4848be
Fiachra Merwick <fiachra.merwick@gmail.com>
2020-08-05 00:58:33 +0100

    Update

--- Milestone Project 2 - War Card Game/War.py
+++ Milestone Project 2 - War Card Game/War.py
@@ -1,7 +1,7 @@
 import random
 
 suits=('Hearts', 'Clubs', 'Diamonds', 'Spades')
-ranks=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten', 'Jack', 'Queen', 'King', 'Ace')
+ranks=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten', 'Jack', 'Queen', 'King', 'Ace', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 class Card:
@@ -27,6 +27,28 @@ class Deck():
     def shuffle(self):
         random.shuffle(self.all_cards)
 
+    def deal_one(self):
+        return self.all_cards.pop()
+
+class Player():
+
+    def __init__(self, name):
+        self.name=name
+        self.all_cards=[]
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+            
 mydeck=Deck()
 print(mydeck.all_cards[0])
 mydeck.shuffle()
